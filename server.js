66394c04b52a1e06d313a3348fc6a99b8b316956
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-05-13 17:10:26 -0300

    Trying to deploy with heroku...

--- server.js
+++ server.js
@@ -10,18 +10,18 @@ const app = express();
 
 const path = require('path');
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  };
-};
+//const forceSSL = function() {
+//  return function (req, res, next) {
+//    if (req.headers['x-forwarded-proto'] !== 'https') {
+//      return res.redirect(
+//       ['https://', req.get('Host'), req.url].join('')
+//      );
+//    }
+//    next();
+//  };
+//};
 
-app.use(forceSSL());
+//app.use(forceSSL());
 
 app.use(express.static(__dirname + '/dist'));
 
