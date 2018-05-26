9de2da9e6de04bcd4f96dce17791db23ad36f461
Mauro Bonfietti <mauro.bonfietti@gmail.com>
2018-05-26 19:32:23 -0300

    Created Game Component.

---
+++ src/app/game.component.ts
@@ -0,0 +1,66 @@
+import {Component} from '@angular/core';
+import {words} from './words';
+
+@Component({
+    selector: 'game',
+    templateUrl: './game.component.html'
+})
+export class GameComponent
+{
+    word = '';
+    hiddenWord = '';
+    tries = 0;
+    win = false;
+    lost = false;
+
+    letters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
+
+    constructor() {
+        var randomNumber = Math.floor(Math.random() * words.length);
+        this.word = words[randomNumber];
+        this.hiddenWord = '_ '.repeat(this.word.length);
+    }
+
+    check(letter: string) {
+        if ((<HTMLInputElement> document.getElementById('btn-' + letter)) === null) {
+            return true;
+        }
+        if ((<HTMLInputElement> document.getElementById('btn-' + letter)).disabled === true) {
+            return true;
+        }
+        (<HTMLInputElement> document.getElementById('btn-' + letter)).disabled = true;
+        if (this.word.indexOf(letter) === -1) {
+            this.tries++;
+        }
+        const hiddenWordArray = this.hiddenWord.split(' ');
+        for (let i = 0; i < this.word.length; i++) {
+            if (this.word[i] === letter) {
+                hiddenWordArray[i] = letter;
+            }
+        }
+        this.hiddenWord = hiddenWordArray.join(' ');
+        this.checkGame();
+    }
+
+    checkGame() {
+        const wordArray = this.hiddenWord.split(' ');
+        const wordCheck = wordArray.join('');
+        if (wordCheck === this.word) {
+            this.win = true;
+        }
+        if (this.tries >= 9) {
+            this.lost = true;
+        }
+    }
+
+    reload() {
+        location.reload();
+    }
+
+    onKey(event: KeyboardEvent) {
+        this.check(event.key.toUpperCase());
+    }
+}
